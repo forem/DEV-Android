7bdf47af1e1c49695a595ef4418bda738de8ce05
Sergei Munovarov <serhuz@ymail.com>
2018-06-23 23:03:27 +0300

    Add sources

---
+++ app/src/androidTest/java/to/dev/android/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package to.dev.android
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun showWebView() {
+        onView(withId(R.id.webview)).check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/main/java/to/dev/android/MainActivity.kt
@@ -0,0 +1,16 @@
+package to.dev.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webview.loadUrl("https://dev.to")
+    }
+
+}
