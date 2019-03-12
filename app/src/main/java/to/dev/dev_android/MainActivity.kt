5245ea6f683da47b254198bb2726d79cc50ae851
Mac Siri <krairit.siri@gmail.com>
2019-03-12 15:53:26 -0400

    Implement barebone webview

--- app/src/main/java/to/dev/dev_android/MainActivity.kt
+++ app/src/main/java/to/dev/dev_android/MainActivity.kt
@@ -1,12 +1,33 @@
 package to.dev.dev_android
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import to.dev.dev_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        binding.webview.loadUrl(getString(R.string.main_url))
     }
+
+    override fun onBackPressed() {
+        if (binding.webview.canGoBack()) {
+            binding.webview.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
