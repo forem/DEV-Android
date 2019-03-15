2ef96f170a6dec650e5e46405fad184c7999dedd
Mac Siri <krairit.siri@gmail.com>
2019-03-15 17:23:54 -0400

    Update webview config

--- app/src/main/java/to/dev/dev_android/MainActivity.kt
+++ app/src/main/java/to/dev/dev_android/MainActivity.kt
@@ -1,10 +1,8 @@
 package to.dev.dev_android
 
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.support.v7.app.AppCompatActivity
 import to.dev.dev_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.webview.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url)
-                return true
-            }
-        }
+
+        binding.webview.settings.javaScriptEnabled = true
+        binding.webview.settings.domStorageEnabled = true
+
+
         binding.webview.loadUrl(getString(R.string.main_url))
     }
 
