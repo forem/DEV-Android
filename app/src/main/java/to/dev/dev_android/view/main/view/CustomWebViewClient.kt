c70ff9e43da0acbdb3796c0fb356a5c1b8fefb43
Mac Siri <krairit.siri@gmail.com>
2019-04-15 16:58:19 -0400

    Add browser helper

--- app/src/main/java/to/dev/dev_android/view/main/view/CustomWebViewClient.kt
+++ app/src/main/java/to/dev/dev_android/view/main/view/CustomWebViewClient.kt
@@ -5,12 +5,20 @@ import android.net.Uri
 import android.support.customtabs.CustomTabsIntent
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+
+
 
 class CustomWebViewClient(val context: Context) : WebViewClient() {
     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-        if (url.startsWith("https://dev.to")) {
+        if (url.contains("://dev.to")) {
             return false;
         } else {
+            if(url.contains("api.twitter.com/oauth") or url.contains("github.com/login")) {
+                openBrowser(url)
+                return true
+            }
             val builder = CustomTabsIntent.Builder()
             builder.setToolbarColor(-0x1000000)
             val customTabsIntent = builder.build()
@@ -18,5 +26,11 @@ class CustomWebViewClient(val context: Context) : WebViewClient() {
             return true;
         }
 
+    }
+
+    private fun openBrowser(url: String): Boolean {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        context.startActivity(intent)
+        return true
     }
 }
