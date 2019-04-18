850b4720679ac28cdc6bf8db21bf4583b4adf5de
Mac Siri <krairit.siri@gmail.com>
2019-04-18 12:53:09 -0400

    Restructure test folders

---
+++ app/src/test/java/to/dev/dev_android/view/main/view/CustomWebViewClientTest.kt
@@ -0,0 +1,24 @@
+package to.dev.dev_android.view.main.view
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CustomWebViewClientTest {
+
+    @Test
+    fun onPageFinished() {
+    }
+
+    @Test
+    fun shouldOverrideUrlLoading() {
+    }
+
+    @Test
+    fun getContext() {
+    }
+
+    @Test
+    fun getBinding() {
+    }
+}
---
+++ app/src/test/java/to/dev/dev_android/view/main/view/MainActivityTest.kt
@@ -0,0 +1,20 @@
+package to.dev.dev_android.view.main.view
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainActivityTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onNewIntent() {
+    }
+
+    @Test
+    fun onBackPressed() {
+    }
+}
