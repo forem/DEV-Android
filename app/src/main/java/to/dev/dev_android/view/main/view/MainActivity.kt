e754697d7f251e9295ac5e144b139dfdc2bebbac
Subbramanian Lakshmanan <subbramanil@gmail.com>
2019-05-01 15:46:49 -0500

    fix: Choose File Option Not Working Issue
    - Adds a **CustomWebChromeClient** that can handle the event to choose file in the webview
    - Updates webview to have a **webChromeClient**
    - Adds a custom listener interface and let **MainActivity** implement it.
    - Creates & Uses Implicit intent to let the Android show a Chooser with the list of apps that can handle the intent
    - Use **ValueCallback** interface to provide the form data for the chosen picture path
    - Renames the intent variable to more meaningful name
    
    *Note:*
    
    The reason to use a listener interface is to let the activity handle creating the intent to launch gallery and handle the result through "*onActivityResult()*" method. This is a simpler approach.

--- app/src/main/java/to/dev/dev_android/view/main/view/MainActivity.kt
+++ app/src/main/java/to/dev/dev_android/view/main/view/MainActivity.kt
@@ -1,14 +1,19 @@
 package to.dev.dev_android.view.main.view
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.webkit.ValueCallback
+import androidx.core.app.ActivityCompat
 import to.dev.dev_android.R
 import to.dev.dev_android.base.activity.BaseActivity
 import to.dev.dev_android.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class MainActivity : BaseActivity<ActivityMainBinding>(), CustomWebChromeClient.CustomListener {
+
+    private var mFilePathCallback: ValueCallback<Array<Uri>>? = null
 
     override fun layout(): Int {
         return R.layout.activity_main
@@ -38,6 +43,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.settings.domStorageEnabled = true
         binding.webView.webViewClient = CustomWebViewClient(this@MainActivity, binding)
+        binding.webView.webChromeClient = CustomWebChromeClient(this@MainActivity, binding, this)
     }
 
     private fun navigateToHome() {
@@ -50,5 +56,38 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    override fun launchGallery(filePathCallback: ValueCallback<Array<Uri>>?) {
+        mFilePathCallback = filePathCallback
+
+        val galleryIntent = Intent()
+        // Show only images, no videos or anything else
+        galleryIntent.type = "image/*"
+        galleryIntent.action = Intent.ACTION_PICK
+
+        // Always show the chooser (if there are multiple options available)
+        ActivityCompat.startActivityForResult(
+            this,
+            Intent.createChooser(galleryIntent, "Select Picture"),
+            PIC_CHOOSER_REQUEST,
+            null    // No additional data
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == PIC_CHOOSER_REQUEST) {
+                if (data != null) {
+                    mFilePathCallback?.onReceiveValue(arrayOf(data.data))
+                    mFilePathCallback = null
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    companion object {
+        private const val PIC_CHOOSER_REQUEST = 100
     }
 }
