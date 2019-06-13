f51eb12be58695784f93b39414307cd7864423b7
Roberto Orozco <issc.roberto@outlook.com>
2019-06-13 14:24:16 -0500

    Clear history when user returns to home

--- app/src/main/java/to/dev/dev_android/view/main/view/CustomWebChromeClient.kt
+++ app/src/main/java/to/dev/dev_android/view/main/view/CustomWebChromeClient.kt
@@ -5,12 +5,15 @@ import android.net.Uri
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import to.dev.dev_android.R
 import to.dev.dev_android.databinding.ActivityMainBinding
 
 
-class CustomWebChromeClient(val context: Context,
-                            val binding: ActivityMainBinding,
-                            private val listener: CustomListener) : WebChromeClient() {
+class CustomWebChromeClient(
+    val context: Context,
+    val binding: ActivityMainBinding,
+    private val listener: CustomListener
+) : WebChromeClient() {
 
     override fun onShowFileChooser(
         webView: WebView?,
@@ -19,6 +22,11 @@ class CustomWebChromeClient(val context: Context,
     ): Boolean {
         listener.launchGallery(filePathCallback)
         return true
+    }
+
+    override fun onProgressChanged(view: WebView, newProgress: Int) {
+        super.onProgressChanged(view, newProgress)
+        if (newProgress == 100 && view.url == context.getString(R.string.base_url)) view.clearHistory()
     }
 
     interface CustomListener {
