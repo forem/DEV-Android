5e96ecf9b448b61fbb10aa35876dd96c5b289578
Harsh Shandilya <msfjarvis@gmail.com>
2019-07-06 02:33:03 +0530

    Uprev Kotlin to 1.3.41
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- buildSrc/src/main/kotlin/to/dev/dev_android/build/Config.kt
+++ buildSrc/src/main/kotlin/to/dev/dev_android/build/Config.kt
@@ -1,7 +1,7 @@
 package to.dev.dev_android.build
 
 object BuildConfig {
-    const val kotlinVersion = "1.3.31"
+    const val kotlinVersion = "1.3.41"
 
     const val minSdkVersion = 18
     const val targetSdkVersion = 28
