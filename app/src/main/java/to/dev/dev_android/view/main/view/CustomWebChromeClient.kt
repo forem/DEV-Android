736702d45bde3f58fbdba33ded696e5660cd9b4b
Roberto Orozco <issc.roberto@outlook.com>
2019-08-19 17:39:22 -0500

    Provide base url to CustomWebChromeClient through constructor

--- app/src/main/java/to/dev/dev_android/view/main/view/CustomWebChromeClient.kt
+++ app/src/main/java/to/dev/dev_android/view/main/view/CustomWebChromeClient.kt
@@ -1,16 +1,14 @@
 package to.dev.dev_android.view.main.view
 
-import android.content.Context
 import android.net.Uri
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
-import to.dev.dev_android.R
 import to.dev.dev_android.databinding.ActivityMainBinding
 
 
 class CustomWebChromeClient(
-    val context: Context,
+    val baseURL: String,
     val binding: ActivityMainBinding,
     private val listener: CustomListener
 ) : WebChromeClient() {
@@ -26,7 +24,9 @@ class CustomWebChromeClient(
 
     override fun onProgressChanged(view: WebView, newProgress: Int) {
         super.onProgressChanged(view, newProgress)
-        if (newProgress == 100 && view.url == context.getString(R.string.base_url)) view.clearHistory()
+        if (newProgress == 100 && view.url == baseURL) {
+            view.clearHistory()
+        }
     }
 
     interface CustomListener {
