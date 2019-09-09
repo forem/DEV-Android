e3ecaf8560e2b8076b49e6c835441605f345fb56
Zuhaib Ahmad <xuhaib_ahmad@yahoo.com>
2019-09-09 12:30:14 +0800

    Add detekt static analysis

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -38,6 +38,8 @@ object Versions {
 
     const val org_jetbrains_kotlin: String = "1.3.41" // available: "1.3.50"
 
+    const val io_gitlab_arturbosch_detekt: String = "1.0.1"
+
     /**
      *
      * See issue 19: How to update Gradle itself?
@@ -55,3 +57,7 @@ object Versions {
 val PluginDependenciesSpec.buildSrcVersions: PluginDependencySpec
     inline get() =
             id("de.fayard.buildSrcVersions").version(Versions.de_fayard_buildsrcversions_gradle_plugin)
+
+val PluginDependenciesSpec.detekt: PluginDependencySpec
+    inline get() =
+        id("io.gitlab.arturbosch.detekt").version(Versions.io_gitlab_arturbosch_detekt)
