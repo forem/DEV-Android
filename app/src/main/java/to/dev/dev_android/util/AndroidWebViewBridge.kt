106743f33e9a2b76d04d82d9d01b16fba632c1fc
Varun Barad <varun@varunbarad.com>
2019-10-17 22:18:45 +0530

    Remove unneeded reference to `Context`.

--- app/src/main/java/to/dev/dev_android/util/AndroidWebViewBridge.kt
+++ app/src/main/java/to/dev/dev_android/util/AndroidWebViewBridge.kt
@@ -1,6 +1,5 @@
 package to.dev.dev_android.util
 
-import android.content.Context
 import android.util.Log
 import android.webkit.JavascriptInterface
 
@@ -8,10 +7,10 @@ import android.webkit.JavascriptInterface
  * This class currently is empty because more methods would be added to it
  * when new bridge functionalities are added.
  */
-class AndroidWebViewBridge(private val context: Context) {
+class AndroidWebViewBridge {
     /**
      * Every method that has to be accessed from web-view needs to be marked with
-     * <code>@JavascriptInterface</code>.
+     * `@JavascriptInterface`.
      * This is currently just a sample method which logs an error to Logcat.
      */
     @JavascriptInterface
