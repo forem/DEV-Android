e54fc7ff1ac6d322a3073d8f17671ad663783432
Nizar Mahmoud <nizar.mah99@gmail.com>
2020-05-25 18:01:52 +0300

    Added NetworkUtils to Check if Device is Online/Offline
    Checking if user is online is done through opening a socket with 8.8.8.8 (google dns). This online check is according to the stackoverflow answer linked below. It is faster than other options.
    
    https://stackoverflow.com/a/27312494/5512292

---
+++ app/src/main/java/to/dev/dev_android/util/network/NetworkUtils.kt
@@ -0,0 +1,29 @@
+package to.dev.dev_android.util.network
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
+typealias NetworkStatusCallback = (isOnline: Boolean) -> Unit
+
+object NetworkUtils {
+    suspend fun isOnline(): Boolean = withContext(Dispatchers.IO) {
+        try {
+            val timeout = 1500
+            val address = InetSocketAddress("8.8.8.8", 80)
+
+            Socket().apply {
+                connect(address, timeout)
+                close()
+            }
+
+            true
+        } catch (e: IOException) {
+            false
+        }
+    }
+
+    suspend fun isOffline(): Boolean = !isOnline()
+}
